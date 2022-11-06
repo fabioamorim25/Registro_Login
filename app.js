a0fee8061ff7d372ba9be22ebf59170b9b6c9b65
Fábio Amorim <fabiosantosamorim2@gmail.com>
2022-11-06 08:36:27 -0300

    Conexão com o banco de dados

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ const router = require('./routes/router');
 const express = require('express');
 const app = express()
 const path= require('path');
+const mongoose = require('mongoose');
 
+//conexão com o banco de dados local-------------------------------------------------------------------
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+let db=mongoose.connection;
+
+db.on("error", ()=>{
+   console.log(error);
+})
+db.once("open", ()=>{
+   console.log("banco de dados carregado");
+})
+//-----------------------------------------
 
 //Definir onde esta os arquivos do sistema
 app.use(express.static(path.join(__dirname,'public')));
@@ -14,9 +26,6 @@ app.set('views engine', 'ejs');
 
 
 app.use('/', router);
-
-
-
 
 
 
