ad326c67d34267869a6976d1c8686ca16a5465b2
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-01-23 08:41:55 -0300

    Funcionalidade de registra usuário

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,25 @@
+const User = require('../models/user'); 
+
+
+
 const Register =  async (req,res)=>{
 
+    const {email} = req.body;
+
+    try {
+       //1° VERIFICAÇÃO: exite usuário com o mesmo email
+        if(await User.findOne({email})){
+            return res.status(400).send({error: "O usuário já existe"})
+        }
+
+        
+       const user = await User.create(req.body);
+       user.password = undefined;//não retornar a senha
+
+        return res.send({user});
+    } catch (error) {
+        return res.status(400).send({error: 'Registro do usuário falhou'})
+    }
     
 
 
