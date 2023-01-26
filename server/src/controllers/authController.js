c4644319851315c9f5e7fb4d1933be369e4d9860
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-01-26 09:44:49 -0300

    Funcionalidade login e register com token

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -3,6 +3,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 
+function generateToken(params={}){
+    //Criar e retornar um token para o registro ou login 
+    return jwt.sign(params , process.env.SECRET,{
+        //tempo para expirar o token: Aqui o tempo é de 1 dia (86400 segundos)
+        expiresIn: process.env.EXPIRE_TOKEN,
+    });
+}
 
 
 const Register =  async (req,res)=>{
@@ -19,7 +26,12 @@ const Register =  async (req,res)=>{
        const user = await User.create(req.body);
        user.password = undefined;//não retornar a senha
 
-        return res.send({user});
+       //retornar o usuario cadastrado e o token
+        return res.send({
+            user, 
+            token:generateToken({id:user.id})
+        });
+
     } catch (error) {
         return res.status(400).send({error: 'Registro do usuário falhou'})
     }
@@ -30,33 +42,33 @@ const Login = async (req,res)=>{
     const {email,password}= req.body;
 
 
-    //pegar o primeiro usuário do banco que possui o mesmo email digitado
-    const user = await User.findOne({email}).select('+password');
+    try {
+        //pegar o primeiro usuário do banco que possui o mesmo email digitado
+        const user = await User.findOne({email}).select('+password');
      
-    //1° VERIFICAÇÃO: O usuário existe no banco
-    if (!user){
-        return res.status(400).send({error: "O usuário não existe"});
-    }
-    //2° VERIFICAÇÃO: Comparar a senha digitada com a senha do banco 
-    if(! await bcrypt.compare(password, user.password)){
-        return res.status(400).send({error:"Password invalido"});
-    }
-    user.password = undefined;//não retornar a senha
 
-
-    //Criar um token para o usuário
-    const token = jwt.sign({id: user.id}, process.env.SECRET,{
-
-        //tempo para expirar o token: Aqui o tempo é de 1 dia (86400 segundos)
-        expiresIn: process.env.EXPIRE_TOKEN,
-
-    });
-
-
-
-
+        //1° VERIFICAÇÃO: O usuário existe no banco
+        if (!user){
+            return res.status(400).send({error: "O usuário não existe"});
+        }
+        //2° VERIFICAÇÃO: Comparar a senha digitada com a senha do banco
+        if(! await bcrypt.compare(password, user.password)){
+            return res.status(400).send({error:"Password invalido"});
+        }
     
-    res.send({user, token});
+        user.password = undefined;//não retornar a senha
+
+
+        //retornar os dados do usuario e o token
+        res.send({
+           user,
+           token:generateToken({id:user.id})
+        })
+
+    } catch (error) {
+        return res.status(400).send({error: 'Falha no login'})
+    }
+    
 }
 
 module.exports = {Register,Login}
