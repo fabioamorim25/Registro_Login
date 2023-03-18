2c552b628bdbbbb95b3c9ecfa986d787a3af1791
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-03-18 10:28:12 -0300

    Layout da pagina de register

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -19,27 +19,29 @@ function RegisterPage() {
   
   
   return (
-    <div>
-      <header>
-        <h2>AmorimPg</h2>
-        <h4>faça registro e comece a usar</h4>
-      </header>
+    <div className='containerFrom'>
+      <div className='fromBox'>
+        <header>
+          <h2>AmorimPg</h2>
+          <h4>faça registro e comece a usar</h4>
+        </header>
 
-      <form onSubmit={actionSave}>
-        <input type='text' value={name} placeholder="Digite seu Nome"
-          onChange={(event) => setName(event.target.value)}>
-        </input>
+        <form onSubmit={actionSave}>
+          <input type='text' value={name} placeholder="Digite seu Nome"
+            onChange={(event) => setName(event.target.value)}>
+          </input>
 
-        <input type='email' value={email} placeholder="Digite seu E-mail"
-          onChange={(event) => setEmail(event.target.value)}>
-        </input>
+          <input type='email' value={email} placeholder="Digite seu E-mail"
+            onChange={(event) => setEmail(event.target.value)}>
+          </input>
 
-        <input type='password' value={password} placeholder="Digite sua Senha"
-          onChange={(event) => setPassword(event.target.value)}>
-        </input>
+          <input type='password' value={password} placeholder="Digite sua Senha"
+            onChange={(event) => setPassword(event.target.value)}>
+          </input>
 
-        <button type='submit'>Registar</button>
-      </form>
+          <button type='submit'>Registar</button>
+        </form>
+      </div>
     </div>
   )
 }
